5f7353a0c83e64e8b1ba2573cbf917beec927892
michaelfechner <resetknopf@googlemail.com>
2025-06-25 13:40:01 +0200

    Added Database connection to app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
- 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from data_models import db, Author, Book
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+db.init_app(app)
